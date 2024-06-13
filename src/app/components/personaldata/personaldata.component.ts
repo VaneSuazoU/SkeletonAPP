7784ee12e200ead8294a1568465c95b63ebec3e3
VaneSuazoU <vanessa.suazo@cl.ey.com>
2024-06-12 22:51:09 -0400

    creando componentes

---
+++ src/app/components/personaldata/personaldata.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-personaldata',
+  templateUrl: './personaldata.component.html',
+  styleUrls: ['./personaldata.component.scss'],
+})
+export class PersonaldataComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
